909831bdb86d58eaaa08446958b90c5172309d39
peter szemraj <peterszemraj@gmail.com>
2023-05-21 07:21:23 +0200

    📝 ♻️ improve ease of use
    Signed-off-by: peter szemraj <peterszemraj@gmail.com>

--- compile_gauntlet.py
+++ compile_gauntlet.py
@@ -1,14 +1,28 @@
+"""
+compile_gauntlet.py is a script that compiles the Gauntlet dataset into a single CSV file.
+"""
 import json
-import pandas as pd
-from pathlib import Path
-from tqdm.auto import tqdm
-import fire
 import logging
+from pathlib import Path
 from typing import Optional
-import requests
 from zipfile import ZipFile
 
-logging.basicConfig(level=logging.INFO)
+import fire
+import pandas as pd
+import requests
+from tqdm.auto import tqdm
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+
+KEY_MAPPING = {
+    "run_date": "date",
+    "date-run": "date",
+    "META_date": "date",
+    "huggingface-model-tag": "model_name",
+    "META_huggingface_model": "model_name",
+    "model": "model_name",
+    "METADATA.META_huggingface_model": "model_name",
+}
 
 
 def standardize_keys(json_obj: dict, mapping: dict) -> dict:
@@ -32,25 +46,26 @@ def download_and_extract_data(dropbox_link: str, data_zip_path: Path) -> None:
         zf.extractall("gauntlet")
 
 
-def generate_gauntlet_summary(
-    dropbox_link: str, output_folder: Optional[str] = None
+def export_summary_gauntlet(
+    dropbox_link: str,
+    output_folder: Optional[str] = None,
+    keep_zip: bool = False,
+    save_parquet: bool = False,
 ) -> None:
-    logging.info("Downloading data...")
-    data_zip_path = Path("data.zip")
-    download_and_extract_data(dropbox_link, data_zip_path)
+    """
+    export_summary_gauntlet - export the summary Gauntlet dataset to a CSV data file
 
+    :param str dropbox_link: link to the Dropbox zip file containing the Gauntlet dataset
+    :param Optional[str] output_folder: path to the output folder, defaults to None
+    :param bool save_parquet: whether to save the DataFrame to a parquet file, defaults to False
+    """
+    logging.info("Downloading data...")
+    data_zip_path = Path.cwd() / "data.zip"
+    download_and_extract_data(dropbox_link, data_zip_path)
+    if not keep_zip:
+        data_zip_path.unlink()
     root_dir = Path("gauntlet")
     assert root_dir.exists(), "The specified directory does not exist."
-
-    KEY_MAPPING = {
-        "run_date": "date",
-        "date-run": "date",
-        "META_date": "date",
-        "huggingface-model-tag": "model_name",
-        "META_huggingface_model": "model_name",
-        "model": "model_name",
-        "METADATA.META_huggingface_model": "model_name",
-    }
 
     df_list = []
 
@@ -61,7 +76,7 @@ def generate_gauntlet_summary(
         if f_path.suffix == ".json":
             params_dict = {}
             try:
-                with f_path.open("r") as f:
+                with f_path.open("r", encoding="utf-8", errors="ignore") as f:
                     params_dict = json.load(f)
             except json.JSONDecodeError:
                 logging.error(f"Error loading JSON file: {f_path}")
@@ -96,21 +111,22 @@ def generate_gauntlet_summary(
 
     if not df.empty:
         output_folder = (
-            Path(output_folder) if output_folder else Path.cwd() / "gauntlet-as-dataset"
+            Path(output_folder) if output_folder else Path.cwd() / "as-dataset"
         )
         output_folder.mkdir(exist_ok=True, parents=True)
+        logging.info(f"Saving the summary data to {output_folder}")
         output_csv = output_folder / "gauntlet_summary_data.csv"
-        output_parquet = output_folder / "gauntlet_summary_data.parquet"
         df = df.convert_dtypes()
         print(df.info())
         df.to_csv(output_csv, index=False)
-        df.to_parquet(output_parquet, index=False)
-        logging.info(
-            f"Successfully saved the summary data to {output_csv} and {output_parquet}"
-        )
+        if save_parquet:
+            output_parquet = output_folder / "gauntlet_summary_data.parquet"
+            df.to_parquet(output_parquet, index=False)
+            logging.info(f"Saved the summary data to {output_parquet}")
+        logging.info(f"Done! saved the summary data to {output_csv}")
     else:
         logging.warning("No data found in the specified directory.")
 
 
 if __name__ == "__main__":
-    fire.Fire(generate_gauntlet_summary)
+    fire.Fire(export_summary_gauntlet)
