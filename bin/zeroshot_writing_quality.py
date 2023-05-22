5d4313145e587e60877d494f9c15185ebea1f3d8
peter szemraj <peterszemraj@gmail.com>
2023-05-22 09:23:21 +0200

    🎨
    Signed-off-by: peter szemraj <peterszemraj@gmail.com>

--- bin/zeroshot_writing_quality.py
+++ bin/zeroshot_writing_quality.py
@@ -210,7 +210,10 @@ def process_dataframe(
     df.info(buf=buffer)
     metadata = {
         "input_dataframe": pp.pformat(buffer.getvalue()),
-        "pred_stats": pd.DataFrame(results).convert_dtypes().describe().to_dict(),
+        "pred_stats": pd.DataFrame(results)
+        .apply(pd.to_numeric, errors="coerce")
+        .describe()
+        .to_dict(),
         "model": classifier.model.config.name_or_path,
         "timestamp": get_timestamp(),
     }
